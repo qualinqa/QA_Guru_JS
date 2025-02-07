2d773b3eddf881d3c584d2d05d269561f5dc223b
irina <i.ivanova@mapcard.pro>
2025-02-07 10:55:04 +0300

    updated

--- src/pages/addArticlePage.js
+++ src/pages/addArticlePage.js
@@ -6,7 +6,7 @@ export class AddArticlePage {
     
     this.page = page;
 
-    this.articleTitleField = getByRole('textbox', { name: 'Article Title' });
+    this.articleTitleField = page.getByRole('textbox', { name: 'Article Title' });
     this.describeArticleField = page.getByPlaceholder("What's this article about?");
     this.articleField = page.getByPlaceholder('Write your article');
     this.enterTagsField = page.getByPlaceholder('Enter tags');
