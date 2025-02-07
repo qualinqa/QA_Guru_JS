c288f95092f300b3afc7aa688675195f873ec1e3
irina <i.ivanova@mapcard.pro>
2025-02-07 12:21:34 +0300

    updated

--- playwright.config.js
+++ playwright.config.js
@@ -35,23 +35,21 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-/*
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-*/
+
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-/*
+
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
     
-*/
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
