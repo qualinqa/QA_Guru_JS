9cfa7164c6f0b82ecc7e85cec4f6c889a0a30318
irina <i.ivanova@mapcard.pro>
2025-02-10 14:39:49 +0300

    updated

---
+++ tests/ArticleAction.spec.js
@@ -0,0 +1,94 @@
+import { faker } from '@faker-js/faker';
+import { test, expect } from '@playwright/test';
+import { MainPage, LoginPage, RegisterPage, YourFeedPage, AddArticlePage, ArticlePage }
+ from '../src/pages/index';
+
+
+
+const URL_UI = 'https://realworld.qa.guru/';
+
+const articleData = {
+   newArticleTitle: faker.lorem.sentence(3),
+   newDescribeArticle: "описание",
+   newArticle: "Вот такая интересная статья получилась!",
+   newTag: "просто тэг"
+};
+
+
+// Действия пользователя со статьей
+
+test.describe('Действия пользователя со статьей', () => {
+   
+
+   test.beforeEach('Пользователь логинится', async ({page}) => {
+      
+      
+      const mainPage = new MainPage (page);
+      const loginPage = new LoginPage(page);
+      await mainPage.open(URL_UI);
+      await mainPage.gotoLogin();
+
+      const oldUser = {
+         username: 'Irishka',
+         email: 'Irishka@ya.ru',
+         password: 'Irishka'
+       }
+
+      await loginPage.loginUser(oldUser.email, oldUser.password);
+      
+      
+   });
+
+   test('Пользователь может добавить новую статью', async ({ page }) => {
+      
+      
+      const yourFeedPage = new YourFeedPage(page);
+      const addArticlePage = new AddArticlePage(page, articleData.newArticleTitle);
+      const articlePage = new ArticlePage(page);
+      
+      
+      await yourFeedPage.gotoNewArticle();
+      
+      await addArticlePage.publishNewArticle(
+         articleData.newArticleTitle,
+         articleData.newDescribeArticle,
+         articleData.newArticle, 
+         articleData.newTag);
+
+     
+      
+      await expect(addArticlePage.checkArticleTitleField).toContainText(articleData.newArticleTitle);
+
+      });
+
+      
+
+      test('Пользователь может добавить комментарий к статье', async ({ page }) => {
+      
+      
+   
+      const yourFeedPage = new YourFeedPage(page);
+      const addArticlePage = new AddArticlePage(page, articleData.newArticleTitle);
+      const articlePage = new ArticlePage(page);
+      
+     
+      await yourFeedPage.gotoNewArticle();
+    
+      await addArticlePage.publishNewArticle(
+         articleData.newArticleTitle,
+         articleData.newDescribeArticle,
+         articleData.newArticle,
+         articleData.newTag);
+      
+      
+      await expect(addArticlePage.checkArticleTitleField).toContainText(articleData.newArticleTitle);
+      
+      const textComment = 'Мой комментарий - лучший! Ай да я!'
+      await articlePage.postNewComment(textComment);
+      await expect(articlePage.commentField).toContainText(textComment);
+      
+      
+      });
+      
+      
+});
--- tests/User.spec.js
+++ tests/User.spec.js
@@ -1,25 +1,12 @@
 import { faker } from '@faker-js/faker';
 import { test, expect } from '@playwright/test';
-import { MainPage } from '../src/pages/mainPage';
-import { LoginPage } from '../src/pages/loginPage';
-import { YourFeedPage } from '../src/pages/yourfeedPage';
-import { AddArticlePage} from '../src/pages/addArticlePage';
-import { ArticlePage } from '../src/pages/articlePage';
-import { RegisterPage } from '../src/pages/registerPage';
-import { SettingsPage } from '../src/pages/settingsPage';
 import { UserBuilder } from '../src/helpers/builder/index';
+import { MainPage, LoginPage, RegisterPage, YourFeedPage, RegisterPage, SettingsPage }
+ from '../src/pages/index';
 
 
 
 const URL_UI = 'https://realworld.qa.guru/';
-
-const articleData = {
-   newArticleTitle: faker.lorem.sentence(3),
-   newDescribeArticle: "описание",
-   newArticle: "Вот такая интересная статья получилась!",
-   newTag: "просто тэг"
-};
-
 
 // Пользователь
 
@@ -128,80 +115,3 @@ test('Пользователь может изменить пароль', async 
    
    });
 
-// Действия пользователя со статьей
-
-test.describe('Действия пользователя со статьей', () => {
-   
-
-   test.beforeEach('Пользователь логинится', async ({page}) => {
-      
-      
-      const mainPage = new MainPage (page);
-      const loginPage = new LoginPage(page);
-      await mainPage.open(URL_UI);
-      await mainPage.gotoLogin();
-
-      const oldUser = {
-         username: 'Irishka',
-         email: 'Irishka@ya.ru',
-         password: 'Irishka'
-       }
-
-      await loginPage.loginUser(oldUser.email, oldUser.password);
-      
-      
-   });
-
-   test('Пользователь может добавить новую статью', async ({ page }) => {
-      
-      
-      const yourFeedPage = new YourFeedPage(page);
-      const addArticlePage = new AddArticlePage(page, articleData.newArticleTitle);
-      const articlePage = new ArticlePage(page);
-      
-      
-      await yourFeedPage.gotoNewArticle();
-      
-      await addArticlePage.publishNewArticle(
-         articleData.newArticleTitle,
-         articleData.newDescribeArticle,
-         articleData.newArticle, 
-         articleData.newTag);
-
-     
-      
-      await expect(addArticlePage.checkArticleTitleField).toContainText(articleData.newArticleTitle);
-
-      });
-
-      
-
-      test('Пользователь может добавить комментарий к статье', async ({ page }) => {
-      
-      
-   
-      const yourFeedPage = new YourFeedPage(page);
-      const addArticlePage = new AddArticlePage(page, articleData.newArticleTitle);
-      const articlePage = new ArticlePage(page);
-      
-     
-      await yourFeedPage.gotoNewArticle();
-    
-      await addArticlePage.publishNewArticle(
-         articleData.newArticleTitle,
-         articleData.newDescribeArticle,
-         articleData.newArticle,
-         articleData.newTag);
-      
-      
-      await expect(addArticlePage.checkArticleTitleField).toContainText(articleData.newArticleTitle);
-      
-      const textComment = 'Мой комментарий - лучший! Ай да я!'
-      await articlePage.postNewComment(textComment);
-      await expect(articlePage.commentField).toContainText(textComment);
-      
-      
-      });
-      
-      
-});
